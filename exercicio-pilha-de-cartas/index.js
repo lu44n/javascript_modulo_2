96ee023c0662bb999fa57707540e647ce5d38231
Mikael <99888430+lu44n@users.noreply.github.com>
2024-04-30 11:59:44 -0300

    Exercicios
    Exercicio Pilha de Cartas

---
+++ exercicio-pilha-de-cartas/index.js
@@ -0,0 +1,26 @@
+let cartas = []
+alert("Sistema inicializado com sucesso!")
+let num_cartas = 0
+let opc_menu = prompt("Numero de cartas no baralho: " + num_cartas + "\n\nEscolha uma das opções abaixo:\n1. Adicionar uma carta\n2. Puxar uma carta\n3. Sair")
+
+while (opc_menu != 3){
+    switch (opc_menu) {
+        case "1":
+            const carta = prompt("Insira a carta: ")
+            cartas.push(carta)
+            num_cartas = cartas.length
+            alert("Carta adicionada com sucesso!")
+            break;
+        case "2":
+            const carta_puxada = cartas.pop()
+            alert("A carta " + carta_puxada + " foi puxada com sucesso!")
+            break;
+        case "3":
+            alert("Obrigado por utilizar nossos serviços!")
+            break;
+        default:
+            alert("Opção inválida!")
+            break;
+    }
+    opc_menu = prompt("Numero de cartas no baralho: " + num_cartas + "\n\nEscolha uma das opções abaixo:\n1. Adicionar uma carta\n2. Puxar uma carta\n3. Sair")
+}
