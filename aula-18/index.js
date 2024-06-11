2c0c97588f5c4f2c36605c00cab0c567c031e9b5
Mikael <99888430+lu44n@users.noreply.github.com>
2024-06-11 14:42:05 -0300

    Aula 18

---
+++ aula-18/index.js
@@ -0,0 +1,13 @@
+// Métodos são funções atreladas a objetos
+
+let pessoa = {
+    nome: "Mikael",
+    idade: 20,
+    dizerOla() {
+        console.log("Olá, mundo! Meu nome é " + this.nome + "!")
+    }
+}
+
+console.log(pessoa)
+
+pessoa.dizerOla()
