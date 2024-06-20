c5b59366721c51f45a05c0e787c808cc449f9fcb
Mikael <99888430+lu44n@users.noreply.github.com>
2024-06-20 14:17:02 -0300

    Aula 25

---
+++ aula-25/index.js
@@ -0,0 +1,13 @@
+// Objetos globais podem ser usados para criar e converter valores!
+
+let pessoa = Object()
+
+let lista = Array("A", "B", "C")
+
+let a = String(42)
+
+let b = Number("42")
+
+let c = Date()
+
+let pi = Math.PI
